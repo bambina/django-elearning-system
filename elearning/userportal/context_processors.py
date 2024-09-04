70605ff06e6dad8bbdecc3cec1837157f1e59a01
Bambi <mayuko.hirono@gmail.com>
2024-09-04 22:53:34 +0900

    Format unread_notifications functions

--- elearning/userportal/context_processors.py
+++ elearning/userportal/context_processors.py
@@ -1,9 +1,10 @@
 from .models import Notification
+from django.http import HttpRequest
+from typing import Dict, Any
 
 
-def unread_notifications(request):
-    # print("unread_notifications is called")
-    # print(request.context)
+def unread_notifications(request: HttpRequest) -> Dict[str, Any]:
+    """Count of unread notifications for the request user."""
     if not request.user.is_authenticated:
         return {}
 
