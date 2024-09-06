e1f11b7549140448dbb4cd5d59bd06f0a9ed63b4
Bambi <mayuko.hirono@gmail.com>
2024-09-06 15:10:15 +0900

    Add tests for shared tasks

---
+++ elearning/userportal/tests/test_tasks.py
@@ -0,0 +1,47 @@
+from unittest.mock import patch
+
+from django.test import TestCase
+from django.core.exceptions import ObjectDoesNotExist
+
+from userportal.tasks import *
+from userportal.tests.model_factories import *
+
+
+class SharedTaskTest(TestCase):
+    """Tests for Celery's shared tasks."""
+
+    @patch('userportal.tasks.delete_qa_questions.apply_async')
+    def test_delete_qa_questions(self, mock_apply_async):
+        # Prepare test data
+        room_name_1, room_name_2 = "room1", "room2"
+        qa_question1 = QAQuestionFactory.create(room_name=room_name_1)
+        qa_question2 = QAQuestionFactory.create(room_name=room_name_2)
+
+        # Mock the apply_async method to call the actual function
+        mock_apply_async.side_effect = lambda args, **kwargs: delete_qa_questions(*args)
+        # Call the function
+        delete_qa_questions.apply_async(args=[room_name_1])
+
+        # Check if the question was deleted
+        with self.assertRaises(ObjectDoesNotExist):
+            qa_question1.refresh_from_db()
+        self.assertFalse(QAQuestion.objects.filter(room_name=room_name_1).exists())
+        # Check if the other question was not deleted
+        qa_question2.refresh_from_db()
+        self.assertTrue(QAQuestion.objects.filter(room_name=room_name_2).exists())
+
+
+    @patch('userportal.tasks.mark_notifications_as_read.apply_async')
+    def test_mark_notifications_as_read(self, mock_apply_async):
+        # Prepare test data
+        notification = NotificationFactory.create(message="Notification", is_read=False)
+        notification_ids = [notification.id]
+
+        # Mock the apply_async method to call the actual function
+        mock_apply_async.side_effect = lambda args, **kwargs: mark_notifications_as_read(*args)
+        # Call the function
+        mark_notifications_as_read.apply_async(args=[notification_ids])
+
+        # Check if the notification was marked as read
+        notification.refresh_from_db()
+        self.assertTrue(notification.is_read)
