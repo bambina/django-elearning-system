7c3559ff609a505e05a8f729b716ec891727227e
Bambi <mayuko.hirono@gmail.com>
2024-09-07 12:16:01 +0900

    Refactoring

--- elearning/userportal/tests/test_tasks.py
+++ elearning/userportal/tests/test_tasks.py
@@ -21,18 +21,20 @@ class SharedTaskTest(TestCase):
         shutil.rmtree(TEST_MEDIA_ROOT, ignore_errors=True)
         super().tearDownClass()
 
-    @patch("userportal.tasks.delete_qa_questions.apply_async")
-    def test_delete_qa_questions(self, mock_apply_async):
+    @patch("userportal.tasks.delete_qa_questions.delay")
+    def test_delete_qa_questions(self, mock_delay):
         # Prepare test data
         room_name_1, room_name_2 = "room1", "room2"
         qa_question1 = QAQuestionFactory.create(room_name=room_name_1)
         qa_question2 = QAQuestionFactory.create(room_name=room_name_2)
 
-        # Mock the apply_async method to call the actual function
-        mock_apply_async.side_effect = lambda args, **kwargs: delete_qa_questions(*args)
+        # Mock the delay method to call the actual function
+        mock_delay.side_effect = lambda room_name: delete_qa_questions(room_name)
         # Call the function
-        delete_qa_questions.apply_async(args=[room_name_1])
+        delete_qa_questions.delay(room_name_1)
 
+        # Check if the delay method was called with the correct arguments
+        mock_delay.assert_called_once_with(room_name_1)
         # Check if the question was deleted
         with self.assertRaises(ObjectDoesNotExist):
             qa_question1.refresh_from_db()
@@ -41,21 +43,23 @@ class SharedTaskTest(TestCase):
         qa_question2.refresh_from_db()
         self.assertTrue(QAQuestion.objects.filter(room_name=room_name_2).exists())
 
-    @patch("userportal.tasks.notify_students_of_live_qa_start.apply_async")
-    def test_notify_students_of_live_qa_start(self, mock_apply_async):
+    @patch("userportal.tasks.notify_students_of_live_qa_start.delay")
+    def test_notify_students_of_live_qa_start(self, mock_delay):
         # Prepare test data
         current_term = AcademicTermFactory.create()
         offering = CourseOfferingFactory.create(term=current_term)
         enrollments = EnrollmentFactory.create_batch(2, offering=offering)
         course_id = enrollments[0].offering.course.id
 
-        # Mock the apply_async method to call the actual function
-        mock_apply_async.side_effect = (
-            lambda args, **kwargs: notify_students_of_live_qa_start(*args)
+        # Mock the delay method to call the actual function
+        mock_delay.side_effect = lambda course_id: notify_students_of_live_qa_start(
+            course_id
         )
         # Call the function
-        notify_students_of_live_qa_start.apply_async(args=[course_id])
+        notify_students_of_live_qa_start.delay(course_id)
 
+        # Check if the delay method was called with the correct arguments
+        mock_delay.assert_called_once_with(course_id)
         # Check if the notifications were created
         for e in enrollments:
             notification = Notification.objects.filter(user=e.student.user).first()
@@ -69,62 +73,72 @@ class SharedTaskTest(TestCase):
             },
         }
     )
-    @patch("userportal.tasks.notify_students_of_material_creation.apply_async")
-    def test_notify_students_of_material_creation(self, mock_apply_async):
+    @patch("userportal.tasks.notify_students_of_material_creation.delay")
+    def test_notify_students_of_material_creation(self, mock_delay):
         # Prepare test data
         offering = CourseOfferingFactory.create()
         material = MaterialFactory.create(course=offering.course)
         enrollments = EnrollmentFactory.create_batch(2, offering=offering)
 
-        # Mock the apply_async method to call the actual function
-        mock_apply_async.side_effect = (
-            lambda args, **kwargs: notify_students_of_material_creation(*args)
+        # Mock the delay method to call the actual function
+        mock_delay.side_effect = (
+            lambda course_id, material_id: notify_students_of_material_creation(
+                course_id, material_id
+            )
         )
         # Call the function
-        notify_students_of_material_creation.apply_async(
-            args=[offering.course.id, material.id]
-        )
+        notify_students_of_material_creation.delay(offering.course.id, material.id)
 
+        # Check if the delay method was called with the correct arguments
+        mock_delay.assert_called_once_with(offering.course.id, material.id)
         # Check if the notifications were created
         for e in enrollments:
             notification = Notification.objects.filter(user=e.student.user).first()
             self.assertIsNotNone(notification)
             self.assertIn("new material", notification.message)
 
-    @patch("userportal.tasks.notify_teacher_of_new_enrollment.apply_async")
-    def test_notify_teacher_of_new_enrollment(self, mock_apply_async):
+    @patch("userportal.tasks.notify_teacher_of_new_enrollment.delay")
+    def test_notify_teacher_of_new_enrollment(self, mock_delay):
         # Prepare test data
         teacher = TeacherProfileFactory.create()
         offering = CourseOfferingFactory.create(course__teacher=teacher)
         enrollment = EnrollmentFactory.create(offering=offering)
 
-        # Mock the apply_async method to call the actual function
-        mock_apply_async.side_effect = (
-            lambda args, **kwargs: notify_teacher_of_new_enrollment(*args)
+        # Mock the delay method to call the actual function
+        mock_delay.side_effect = (
+            lambda course_id, offering_id, username: notify_teacher_of_new_enrollment(
+                course_id, offering_id, username
+            )
         )
         # Call the function
-        notify_teacher_of_new_enrollment.apply_async(
-            args=[offering.course.id, offering.id, enrollment.student.user.username]
+        notify_teacher_of_new_enrollment.delay(
+            offering.course.id, offering.id, enrollment.student.user.username
         )
 
+        # Check if the delay method was called with the correct arguments
+        mock_delay.assert_called_once_with(
+            offering.course.id, offering.id, enrollment.student.user.username
+        )
         # Check if the notification was created
         notification = Notification.objects.filter(user=teacher.user).first()
         self.assertIsNotNone(notification)
         self.assertIn("enrolled", notification.message)
 
-    @patch("userportal.tasks.mark_notifications_as_read.apply_async")
-    def test_mark_notifications_as_read(self, mock_apply_async):
+    @patch("userportal.tasks.mark_notifications_as_read.delay")
+    def test_mark_notifications_as_read(self, mock_delay):
         # Prepare test data
         notifications = NotificationFactory.create_batch(2, is_read=False)
         notification_ids = [n.id for n in notifications]
 
-        # Mock the apply_async method to call the actual function
-        mock_apply_async.side_effect = (
-            lambda args, **kwargs: mark_notifications_as_read(*args)
+        # Mock the delay method to call the actual function
+        mock_delay.side_effect = lambda notification_ids: mark_notifications_as_read(
+            notification_ids
         )
         # Call the function
-        mark_notifications_as_read.apply_async(args=[notification_ids])
+        mark_notifications_as_read.delay(notification_ids)
 
+        # Check if the delay method was called with the correct arguments
+        mock_delay.assert_called_once_with(notification_ids)
         # Check if the notification was marked as read
         for n in notifications:
             n.refresh_from_db()
